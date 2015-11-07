b6e54464f996b6f078ea4afff4232129d72def46
Po Chen <chenpaul914@gmail.com>
2015-11-07 23:02:17 +1100

    babel 6 and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -10,58 +10,51 @@ let cssVars = require('postcss-simple-vars');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let ExtractCSS = new ExtractTextPlugin(1, 'stylesheets/application.css');
 
-let context = process.env.NODE_ENV || 'development';
-
-let configFns = {
-  development: devConfig,
-  production: prodConfig
-};
-
-let config = configFns[context]();
-
-module.exports = config;
-
-function devConfig() {
-  return {
-    context: path.resolve(__dirname, 'input'),
-    entry: {
-      application: './javascripts/app.js',
-      styles: './stylesheets/app.css'
-    },
-    output: {
-      path: path.resolve(__dirname, 'output'),
-      filename: 'javascripts/[name].js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.html$/,
-          loader: 'ngtemplate?relativeTo=' + path.resolve(__dirname, 'input', 'templates') + '/!html'
-        },
-        {
-          test: /\.js$/,
-          loaders: ['ng-annotate', 'babel']
-        },
-        {
-          test: /\.css$/,
-          loader: ExtractCSS.extract('style', 'css!postcss')
-        }
-      ]
-    },
-    postcss: (webpack) => [
-      cssImport({ addDependencyTo: webpack }),
-      cssNested(),
-      cssVars(),
-      autoprefixer
+module.exports = {
+  context: path.resolve(__dirname, 'input'),
+  entry: {
+    application: './javascripts/app.js',
+    styles: './stylesheets/app.css'
+  },
+  output: {
+    path: path.resolve(__dirname, 'output'),
+    filename: 'javascripts/[name].js'
+  },
+  module: {
+    preLoaders: [
+      { test: /\.js$/, loader: 'eslint-loader' }
     ],
-    plugins: [ExtractCSS],
-    watchOptions: {
-      poll: true,
-      aggregateTimeout: 1000
-    }
-  };
-}
-
-function prodConfig() {
-  throw 'not implemented yet';
-}
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'ngtemplate?relativeTo=' + path.resolve(__dirname, 'input', 'templates') + '/!html'
+      },
+      {
+        test: /\.js$/,
+        loaders: ['ng-annotate', 'babel?presets[]=es2015']
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractCSS.extract('style', 'css!postcss')
+      }
+    ]
+  },
+  eslint: {
+    configFile: './.eslintrc',
+    emitWarning: true,
+    emitError: true,
+    failOnWarning: true,
+    failOnError: true
+  },
+  postcss: (webpack) => [
+    cssImport({ addDependencyTo: webpack }),
+    cssNested(),
+    cssVars(),
+    autoprefixer
+  ],
+  plugins: [ExtractCSS],
+  watchOptions: {
+    poll: true,
+    aggregateTimeout: 1000
+  }
+};
